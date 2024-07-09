544b460bfd7aab70cbe7e75094fd470fd7f45064
〽️iraya <mirayaabrodi@gmail.com>
2024-07-09 14:48:47 +0200

    feat: create loding spinner component

---
+++ src/components/core/loading-spinner/loading-spinner.tsx
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+export default function LoadingSpinner() {
+  return (
+    <div className="spinner">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
