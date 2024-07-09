f36e280edb936fcff465736168ba54d60b012f13
〽️iraya <mirayaabrodi@gmail.com>
2024-07-09 14:49:35 +0200

    feat: create util file

---
+++ src/utils/index.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import clsx, { type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+// this function is for reusable components, e.g buttons
+export function cn(...inputs: ClassValue[]): any {
+  return twMerge(clsx(inputs));
+}
