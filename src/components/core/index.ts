c86c9858c440e3badd5046dae174fbb4656364c5
〽️iraya <mirayaabrodi@gmail.com>
2024-07-09 14:49:09 +0200

    feat: create wrapper component

---
+++ src/components/core/index.ts
@@ -0,0 +1,2 @@
+export * from "./loading-spinner/loading-spinner";
+export * from "./wrapper/wrapper";
---
+++ src/components/core/wrapper/wrapper.tsx
@@ -0,0 +1,20 @@
+import React, { type HTMLAttributes } from "react";
+import { cn } from "../../../utils";
+
+interface Props extends HTMLAttributes<HTMLDivElement> {
+  children: React.ReactNode;
+}
+
+export default function Wrapper({ children, className, ...props }: Props) {
+  return (
+    <div
+      className={cn(
+        "relative flex h-dvh w-full flex-col items-center justify-center bg-white text-black",
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+}
